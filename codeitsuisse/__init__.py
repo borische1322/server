1f3a814053be7ee47d84b9f9ddfd2cd3f2bea954
borische1322 <71879726+borische1322@users.noreply.github.com>
2021-09-25 21:02:11 +0800

    Update __init__.py

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -2,5 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.asteroid
-import codeitsuisse.routes.parasite
 import codeitsuisse.routes.stockhunter
